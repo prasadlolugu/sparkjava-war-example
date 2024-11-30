6b36b7e396f739af390e7d8fd30254bcdb8e675b
yaswanthlolugu <153537429+prasadlolugu@users.noreply.github.com>
2024-11-30 12:27:50 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "hi jilan mama code working");
+		get("/hello", (req, res) -> "hi jilan basha webhook is working properly");
 	}
 }
